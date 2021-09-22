c71d2f98a8f0a658433656227d01f535b4814653
Biente Dungelmann <57255281+Biente@users.noreply.github.com>
2021-09-22 09:58:43 +0000

    poging tot blokjes maken

--- script.js
+++ script.js
@@ -9,16 +9,32 @@ class Ball {
     this.c = c;
   }
 
+  class Rect {
+    constructor(x, y, w, h, c)
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+
+  }
+
   drawBall() {
     fill(this.c)
     ellipse(this.x, this.y, this.w, this.h);
   }
 }
+drawRect() {
+    fill(this.c)
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
 
-var ball1, ball2, ball3;
+var ball1, rect2, ball3;
 function setup() {
   createCanvas(500, 400);
   ball1 = new Ball(100, 100, 50, 50, 5, 5, "red")
+  rect2 = new Rect(400, 300, 60, 100, "black")
 }
 
 function draw() {
@@ -30,10 +46,10 @@ function draw() {
   } else {
     a = 250;
     b = 200;
-    
+
   }
   fill("green");
   circle(a, b, 50);
-
+  
 }
 
