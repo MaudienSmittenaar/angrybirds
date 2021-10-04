235e5b877f8a8d7810b50d23ea696502b963d623
Maudien Smittenaar <57255283+MaudienSmittenaar@users.noreply.github.com>
2021-10-04 11:53:01 +0000

    start collision

--- script.js
+++ script.js
@@ -31,16 +31,32 @@ class Rect {
     fill(this.c)
     rect(this.x, this.y, this.w, this.h);
   }
+
+  checkCollision(){
+    if(ball1.x < this.x && ball1.y > this.y){
+      this.c = "white";
+    }
+    else{
+      this.c = "red";
+    }
+  }
 }
 
 var ball1, rect2, rect3, rect4, rect5;
+var rects = [];
 function setup() {
   createCanvas(500, 400);
   ball1 = new Ball(100, 315, 50, 50, 0, 0, "green")
-  rect2 = new Rect(400, 340, 60, 60, "white")
-  rect3 = new Rect(400, 280, 60, 60, "white")
-  rect4 = new Rect(400, 220, 60, 60, "white")
-  rect5 = new Rect(400, 160, 60, 60, "white")
+  rect2 = new Rect(400, 340, 60, 60, "white");
+  rect3 = new Rect(400, 280, 60, 60, "white");
+  rect4 = new Rect(400, 220, 60, 60, "white");
+  rect5 = new Rect(400, 160, 60, 60, "white");
+
+  rects.push(rect2);
+  rects.push(rect3);
+  rects.push(rect4);
+  rects.push(rect5);
+
 }
 
 lineX2 = 250
@@ -49,10 +65,13 @@ lineY2 = 315
 function draw() {
   background(220);
   ball1.drawBall();
-  rect2.drawRect();
-  rect3.drawRect();
-  rect4.drawRect();
-  rect5.drawRect();
+  
+  rects.forEach((r) =>{
+    r.drawRect();
+    // checkcollision
+    r.checkCollision()
+  });
+
   if (mouseIsPressed) {
     a = mouseX;
     b = mouseY;
@@ -68,8 +87,6 @@ line(100, 315, width, lineY2);
   if (keyCode === 32) {
     ball1.vx = 5;
     frames = (width - 100) / ball1.vx;
-    // console.log(frames, lineY2);
-    // console.log(315 - lineY2)
     ball1.vy = ((315 - lineY2) / frames) * -1;
   }
 
@@ -79,28 +96,6 @@ line(100, 315, width, lineY2);
 
   if (keyIsDown(DOWN_ARROW)) {
     lineY2 += 2;
-  }
-
-  if (a + 25 > rect2.x && a - 25 < rect2.x + rect2.w) {
-    if (b + 25 > rect2.y && b - 25 < rect2.y + rect2.h)
-      rect2.c = "red";
-    else rect2.c = "white";
-  }
-  if (a + 25 > rect3.x && a - 25 < rect3.x + rect3.w) {
-    if (b + 25 > rect3.y && b - 25 < rect3.y + rect3.h)
-      rect3.c = "red";
-    else rect3.c = "white";
-  }
-  if (a + 25 > rect4.x && a - 25 < rect4.x + rect4.w) {
-    if (b + 25 > rect4.y && b - 25 < rect4.y + rect4.h)
-      rect4.c = "red";
-    else rect4.c = "white";
-  }
-
-  if (a + 25 > rect5.x && a - 25 < rect5.x + rect5.w) {
-    if (b + 25 > rect5.y && b - 25 < rect5.y + rect5.h)
-      rect5.c = "red";
-    else rect5.c = "white";
-  }
+  } 
 }
 
